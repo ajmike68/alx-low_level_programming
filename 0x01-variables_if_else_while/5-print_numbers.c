ef3692f35e5996f5fa99776cec35a009a35338f6
Azeez Ogungbade <ajmike68@yahoo.com>
2022-10-20 08:23:45 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,28 @@
+\#include <stdio.h>
+
+
+/**
+
+ * main - main function
+
+ * Return: Returns 0 if successful
+
+ */
+
+
+int main(void)
+
+{
+  
+  int i;  
+
+  
+  for (i = 0; i < 10; i++)
+    
+    printf("%d", i);
+  
+  printf("\n");
+   
+  return (0);
+  
+}
---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+
+ * main - main function
+
+ * Return: Returns 0 if successful
+
+ */
+
+int main(void)
+  
+{
+  
+  int i;
+  
+  
+  for (i = 0; i < 10; i++)
+    
+    putchar(i + '0');
+  
+  putchar('\n');
+  
+  
+  return (0);
+  
+}
