67c4ed6e1ab7a7922aacdaf7e1962d6be32f0a5c
Azeez Ogungbade <ajmike68@yahoo.com>
2022-10-27 05:16:38 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* main - prints result
+* Return: 0 When successful
+*/
+int main(void)
+{
+int i;
+char putty[] = "_putchar";
+for (i = 0; i < 8; i++)
+{
+_putchar(putty[i]);
+}
+_putchar('\n');
+return (0);
+}
